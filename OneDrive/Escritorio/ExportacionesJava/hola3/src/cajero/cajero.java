851fbc6818b8920ce2d7586ecbd2247a0dca800c
Nicolas Cervantes <jncervantesc@udistrital.edu.co>
2024-06-05 11:31:54 -0500

    prueba2

---
+++ OneDrive/Escritorio/ExportacionesJava/hola3/src/cajero/cajero.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cajero;
+
+/**
+ *
+ * @author Nicolas Cervantes
+ */
+public class cajero {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola otra vez");
+        // TODO code application logic here
+    }
+    
+}
